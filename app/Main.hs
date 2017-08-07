0056d3e6361d6ab11a05bca216c28bd554315470
Matthew Leon <ml@matthewleon.com>
2017-08-07 17:42:33 +0100

    list all modules

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,25 @@
 
 module Main where
 
-import Control.Monad (join)
+import Control.DeepSeq (force)
+import Control.Exception (evaluate)
+import Control.Monad (void, join, unless)
+import Control.Monad.Except (ExceptT(ExceptT), runExceptT, catchError, throwError, liftIO)
 import Data.Bifunctor as Bifunctor
+import Data.Either (rights)
+import Data.Maybe (mapMaybe)
 import Data.Monoid ((<>))
+import Data.Traversable (traverse)
 import qualified Data.SemVer as SemVer
 import qualified Data.Text   as Text
+import qualified Data.Text.Lazy   as TextL
 import qualified Data.Text.IO as TextIO
+import qualified Data.Text.Lazy.IO as TextLIO
 import Data.Text (Text)
 import GHC.IO.Handle (hGetContents)
+import Safe (headMay)
+import System.FilePath.Glob (glob)
+import System.IO (FilePath, IOMode(ReadMode), withFile, stderr)
 import System.IO.Error (catchIOError)
 import System.Exit (ExitCode)
 import System.Process.Text (readProcessWithExitCode)
@@ -18,22 +29,34 @@ import Prelude
 pscPackageMinVersion = SemVer.version 0 2 0 [] []
 
 main :: IO ()
-main = do
-  pscPackageVersion <- getPscPackageVersion
-  case pscPackageVersion of
-    Left err -> TextIO.putStrLn err
-    Right version -> do
-      print version
-      if version >= pscPackageMinVersion
-        then putStrLn "all good"
-        else putStrLn "min version unmet"
-  return ()
+main = void . runExceptT $ do
+  checkPscPackageVersion
+  paths <- ExceptT getPscPackageSourcePaths
+  moduleNames <- liftIO $ rights <$> traverse getModuleName paths
+  liftIO . TextIO.putStr $ Text.unlines moduleNames
+  `catchError` (liftIO . TextIO.hPutStrLn stderr)
+
+checkPscPackageVersion :: ExceptT Text IO ()
+checkPscPackageVersion = do
+  pscPackageVersion <- ExceptT getPscPackageVersion
+  unless (pscPackageVersion >= pscPackageMinVersion) $
+    throwError $
+      "purs-localdocs requires a Minimum psc-package version of "
+      <> SemVer.toText pscPackageMinVersion
+      <> "\nYou are running version " <> SemVer.toText pscPackageVersion
+
+getPscPackageSourcePaths :: IO (Either Text [FilePath])
+getPscPackageSourcePaths = traverse globAll =<< getPscPackageSources
+  where
+  globAll :: [Text] -> IO [FilePath]
+  globAll = fmap concat . traverse (glob . Text.unpack)
 
 getPscPackageVersion :: IO (Either Text SemVer.Version)
-getPscPackageVersion = join . fmap parseSemVer <$> runPscPackageCmd "sources"
+getPscPackageVersion = join . fmap parseSemVer <$> runPscPackageCmd "--version"
   where
-    parseSemVer :: Text -> Either Text SemVer.Version
-    parseSemVer = Bifunctor.first Text.pack . SemVer.fromText
+  parseSemVer :: Text -> Either Text SemVer.Version
+  parseSemVer t = flip Bifunctor.first (SemVer.fromText t) $ \err ->
+    "Error parsing version from psc-package: " <> Text.pack err
 
 getPscPackageSources :: IO (Either Text [Text])
 getPscPackageSources = fmap Text.lines <$> runPscPackageCmd "sources"
@@ -43,8 +66,24 @@ runPscPackageCmd cmd =
   fmap (\(_, out, _) -> Text.stripEnd out) <$>
     catchIOError (Right <$> run) handleErr
   where
-    run :: IO (ExitCode, Text, Text)
-    run = readProcessWithExitCode "psc-package" ["--" ++ cmd] ""
+  run :: IO (ExitCode, Text, Text)
+  run = readProcessWithExitCode "psc-package" [cmd] ""
 
-    handleErr :: IOError -> IO (Either Text (ExitCode, Text, Text))
-    handleErr err = return . Left $ "Error running psc-package: " <> Text.pack (show err)
+  handleErr :: IOError -> IO (Either Text (ExitCode, Text, Text))
+  handleErr err = return . Left $ "Error running psc-package: " <> Text.pack (show err)
+
+getModuleName :: FilePath -> IO (Either Text Text)
+getModuleName fp = withFile fp ReadMode $ \handle -> do
+  -- TODO: handle errors from hGetContents
+  fileContent <- TextLIO.hGetContents handle
+  evaluate . force $ case extractModuleName fileContent of
+    Nothing   -> Left $ "Unable to read module name from " <> Text.pack fp
+    Just name -> Right name
+  where
+  extractModuleName :: TextL.Text -> Maybe Text
+  extractModuleName =
+    headMay . mapMaybe (moduleNameFromLine . TextL.toStrict) . TextL.lines
+    where
+    moduleNameFromLine :: Text -> Maybe Text
+    moduleNameFromLine =
+      fmap (Text.takeWhile (`notElem` ['_', ' '])) . Text.stripPrefix "module "
